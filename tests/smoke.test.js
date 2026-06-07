53f0a9f19b8fa3f609c3bb7ebfc64fe69a7f195e
Ilja Rublevsky <iljarublevsky@MacBook-Pro-Ilya.local>
2026-06-07 21:01:19 +0300

    Смоук-тест: проверяем наличие API кубика вместо снятого плейсхолдера

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -1,5 +1,5 @@
 'use strict';
 var dice = require('../js/dice.js');
 test('dice module loads in Node', function () {
-  assertEqual(dice.__loaded, true);
+  assertEqual(typeof dice.rollDie, 'function');
 });
