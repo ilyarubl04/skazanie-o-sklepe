f1670efef0117e8e6278c359837875a7faeb73a3
Ilja Rublevsky <iljarublevsky@MacBook-Pro-Ilya.local>
2026-06-08 21:27:23 +0300

    SW v11 (карта с самого начала + озвучка новых сцен)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'skazanie-v10';
+const CACHE = 'skazanie-v11';
 const ASSETS = [
   './', './index.html', './css/styles.css',
   './js/dice.js', './js/rules.js', './js/heroes.js', './js/bestiary.js', './js/adventure.js',
