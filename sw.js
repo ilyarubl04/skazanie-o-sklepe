5cd7ccc960a1cd93f110577f7b2aa6ea643b689a
Ilja Rublevsky <iljarublevsky@MacBook-Pro-Ilya.local>
2026-06-08 20:32:19 +0300

    SW v9 (Стадия 2: голем + поворот Акта 2)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'skazanie-v8';
+const CACHE = 'skazanie-v9';
 const ASSETS = [
   './', './index.html', './css/styles.css',
   './js/dice.js', './js/rules.js', './js/heroes.js', './js/bestiary.js', './js/adventure.js',
